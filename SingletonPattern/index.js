0b1fb578e2f74ed50b0eb7c5057cbd15cf0fcc69
jun5e00 <ysj6844@naver.com>
2022-10-06 12:43:18 +0900

    add: Singleton
    #1

---
+++ SingletonPattern/index.js
@@ -0,0 +1,31 @@
+let instance;
+let counter = 0;
+
+class Counter {
+  constructor() {
+    if (instance) {
+      throw new Error("인스턴스는 한개만 생성 가능합니다!");
+    }
+    instance = this;
+  }
+
+  getInstance() {
+    return this;
+  }
+
+  getCount() {
+    return counter;
+  }
+
+  increment() {
+    return ++counter;
+  }
+
+  decrement() {
+    return --counter;
+  }
+}
+
+const singletonCounter = Object.freeze(new Counter());
+
+export default singletonCounter;
